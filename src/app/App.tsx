b91bcc8dd3b49570b1ed2cf8908ac92d3ffce5bb
SurajR2 <surajrasaili190@gmail.com>
2025-06-22 11:36:55 +0545

    Revert "Feature/pricing (#14)"
    This reverts commit 93f0cf62dc2f4ea0d8fa47c3c7d9b7f14d7607a6.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,7 @@
-import { BrowserRouter } from "react-router-dom";
-import { Toaster } from "sonner";
-
-import "@/app/styles/globals.css";
-import AppRoutes from "@/app/AppRoutes";
-import ErrorBoundary from "@/app/ErrorBoundary";
-import { ScrollToTop } from "@/app/hooks/ui/ScrollToTop";
+import "./index.css";
 
 const App = () => {
-  return (
-    <ErrorBoundary>
-      <BrowserRouter>
-        <ScrollToTop />
-        <div>
-          <AppRoutes />
-          <Toaster position="top-right" richColors />
-        </div>
-      </BrowserRouter>
-    </ErrorBoundary>
-  );
+  return <div>App</div>;
 };
 
 export default App;
--- src/app/pages/products/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as SmartSolutions } from "./SmartSolutions";
-export { default as LearnMoreCRM } from "./LearnMoreCRM";
-export { default as ScaleYourBusiness } from "./ScaleYourBusiness";
-export { default as EmpowerBusiness } from "./EmpowerBusiness";
