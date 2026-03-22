8264b201b0ce71d161ccf6406b195d3084ab1320
Milap-Magar <milapmagar12@gmail.com>
2026-03-22 16:49:10 +0545

    mock datasets and overall changes

---
+++ src/app/features/dashboard/admin/pages/contact/ContactAdminPage.ts
@@ -0,0 +1,8 @@
+import React from "react";
+import Contact from "@/app/pages/landing/Contact";
+
+const ContactAdminPage = () => {
+  return React.createElement(Contact, { embedded: true });
+};
+
+export default ContactAdminPage;
